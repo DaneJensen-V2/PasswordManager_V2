1efa2f6ab30ff37d5087a1d1ae095d58a0641996
DaneJensen-V2 <dejense4@asu.edu>
2022-04-05 14:25:04 -0700

    Firebase Setup

--- PasswordManager/AppDelegate.swift
+++ PasswordManager/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Dane Jensen on 4/5/22.
 //
-
+import Firebase
 import UIKit
 
 @main
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
